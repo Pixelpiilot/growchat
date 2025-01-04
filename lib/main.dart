81ce1e54e7591d9019fb26dcfbe2a75d1e4289e8
Deepjit Chowdhury <deepjitchowdhury27@gmail.com>
2025-01-04 09:28:22 +0530

    nav bar add

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:growchat/pages/home.dart';
+import 'package:growchat/pages/navbar.dart';
 
 
 void main() {
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: Home()
+        home: BottomNave(),
     );
   }
 }
